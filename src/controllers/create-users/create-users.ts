31821e7e54d2700c9a3194d08dcb026eaa926769
FelipeBarroso <felipebarroso299@gmail.com>
2024-01-18 09:41:47 +0000

    refactory: add comun interface IController

--- src/controllers/create-users/create-users.ts
+++ src/controllers/create-users/create-users.ts
@@ -1,13 +1,9 @@
 import validator from "validator";
 import { User } from "../../models/user";
-import { HttpRequest, HttpResponse } from "../protocols";
-import {
-  CreateUsersParams,
-  ICreateUserController,
-  ICreateUserRepository,
-} from "./protocols";
+import { HttpRequest, HttpResponse, IController } from "../protocols";
+import { CreateUsersParams, ICreateUserRepository } from "./protocols";
 
-export class CreateUserController implements ICreateUserController {
+export class CreateUserController implements IController {
   constructor(private readonly createUserRepository: ICreateUserRepository) {}
 
   async handle(
--- src/controllers/create-users/protocols.ts
+++ src/controllers/create-users/protocols.ts
@@ -1,11 +1,4 @@
 import { User } from "../../models/user";
-import { HttpRequest, HttpResponse } from "../protocols";
-
-export interface ICreateUserController {
-  handle(
-    HttpRequest: HttpRequest<CreateUsersParams>
-  ): Promise<HttpResponse<User>>;
-}
 
 export interface CreateUsersParams {
   firstName: string;
